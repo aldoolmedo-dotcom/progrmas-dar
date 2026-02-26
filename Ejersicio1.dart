c1e2a5cff9b4a573a96e39bb77677366cdfd565a
aldoolmedo-dotcom <aldo.olmedo@cbtis265.edu.mx>
2026-02-26 14:54:56 -0600

    primeros ejercicios de dart

---
+++ Ejersicio1.dart
@@ -0,0 +1,10 @@
+void main(){
+  String nombre = "Aldo";
+  String direccion = "calle de camino real numero 5";
+  int edfad = 17;
+  double altura = 1.64;
+  bool  esMayor = false;
+  
+  
+  print("Mi nbombre es:$nombre\n Mi direccion es:$direccion\n Tengo: $edfad años\n Mi altura es: $altura\n Estoy soltero o no:$esMayor   ");
+}
---
+++ tipos de datos.dart
@@ -0,0 +1,8 @@
+void main(){
+int edad = 17;
+double precio = 19.99;
+String nombre = "Jose";
+bool esMayor = true;
+var ciudad = "Oaxaca";  
+print(" \n Edad:$edad\n Precio:$precio\n Nombre:$nombre\n Esmayo:$esMayor\n Ciudad:$ciudad");
+}
